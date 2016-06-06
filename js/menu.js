c7e4e9db3bc7f95a96c0a624f4a123699dc745d9
Seth Battis <sethbattis@stmarksschool.org>
2016-06-06 16:21:08 -0400

    Multi-section next/prev
    Now correctly flowing into subsequent sections, wrapping around at the ends of the overall list.

--- js/menu.js
+++ js/menu.js
@@ -16,18 +16,42 @@ var faculty_journal_menu = {
 	},
 	
 	nextStudent: function() {
-		$('#students').val($('#students :selected').next().val());
-		if ($('#students :selected').val() === undefined) {
-			$('#students').val($('#students option:first-child').val());
+		var nextInSection = $('#students :selected').next().val(),
+			firstInNextSection = $('#students :selected').parent().next().find('option:first-child').val(),
+			firstStudent = $('#students option:first-child').val(),
+			next = null;
+			
+		if (nextInSection === undefined) {
+			if (firstInNextSection === undefined) {
+				next = firstStudent;
+			} else {
+				next = firstInNextSection;
+			}
+		} else {
+			next = nextInSection;
 		}
+		
+		$('#students').val(next);
 		this.update();
 	},
 	
 	previousStudent: function() {
-		$('#students').val($('#students :selected').prev().val());
-		if ($('#students :selected').val() === undefined) {
-			$('#students').val($('#students option:last-child').val());
+		var prevInSection = $('#students :selected').prev().val(),
+			lastInPrevSection = $('#students :selected').parent().prev().find('option:last-child').val(),
+			lastStudent = $('#students optgroup:last-child option:last-child').val(),
+			prev = null;
+			
+		if (prevInSection === undefined) {
+			if (lastInPrevSection === undefined) {
+				prev = lastStudent;
+			} else {
+				prev = lastInPrevSection;
+			}
+		} else {
+			prev = prevInSection;
 		}
+		
+		$('#students').val(prev);
 		this.update();
 	}
 }
