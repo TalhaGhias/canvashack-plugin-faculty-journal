f9f9490ea0b3d616b695a2328f1763924379de1a
Seth Battis <sethbattis@stmarksschool.org>
2016-06-08 15:38:53 -0400

    Instance-agnostic

--- menu.php
+++ menu.php
@@ -8,7 +8,7 @@ $smarty = BootstrapSmarty::getSmarty();
 $smarty->addTemplateDir(__DIR__ . '/templates');
 $smarty->addStylesheet('css/menu.css', basename(__FILE__));
 
-$smarty->assign('canvasInstance', $metadata['CANVAS_INSTANCE_URL']);
+$smarty->assign('canvasInstance', $_SESSION['canvasInstanceUrl']);
 $smarty->assign('pluginUrl', $pluginMetadata['PLUGIN_URL']);
 $smarty->assign('user', $_REQUEST['user_id']);
 $smarty->assign('course', $_REQUEST['course_id']);
